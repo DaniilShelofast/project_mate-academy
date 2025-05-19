12dc621fcc3b000bf2431875d61459958984a3eb
Danyil <shelofast.daniil@gmail.com>
2025-05-19 18:00:41 +0300

    add 2 txt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("HELLO");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println(i);
         }
     }
 }
